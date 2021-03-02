083ca7182a4701e4347b1969f131ca5de00702c2
railson carvalho <railson031@hotmail.com>
2021-03-02 18:17:33 -0300

    Special move - Promotion

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -122,7 +122,8 @@ public class ChessMatch {
 			throw new IllegalStateException("There is no piece to be promoted");
 		}
 		if (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
-			return promoted;
+			throw new IllegalStateException("Invalid type for promotion");
+			
 		}
 		
 		Position pos = promoted.getChessPosition().toPosition();
